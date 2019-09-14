109a148d2c7cd1039a89cea1418758901f8f3c08
Victoria Hale <vic.hale74@gmail.com>
2019-09-14 20:04:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -20,13 +20,7 @@ end
 # end
 
 def find_max_value(array)
-  count = 0
-  
-  while count <= array.length
-    puts array.max
-  end
-
-  count += 1 
+  array.index(array.max)
 end 
 
 def find_min_value(array)
