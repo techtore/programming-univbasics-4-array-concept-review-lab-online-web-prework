11e85523bc510dcc22e205003cdf360b782040db
Victoria Hale <vic.hale74@gmail.com>
2019-09-14 20:17:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,14 +14,13 @@ array.length.times do |count|
   if array[count] == value_to_find
     return count
   end
-  
 end 
   return nil
  
 end
 
 def find_max_value(array)
-  x = array[0]
+  x = 0
   array.length.times { |index|
    if array[index] > x 
      x = array[index]
@@ -32,7 +31,7 @@ end
      
 
 def find_min_value(array)
-    x = array[0]
+    x = 0
   array.length.times { |index|
    if array[index] < x 
      x = array[index]
