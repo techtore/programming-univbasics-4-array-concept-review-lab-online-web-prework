dfa16ad63335a275bb1ea71d3b65605e39a06f5a
Victoria Hale <vic.hale74@gmail.com>
2019-09-14 20:16:04 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,24 @@
-def find_element_index(array, value_to_find)
-count = 0 
-  while count < array.length do
-    if array[count] == value_to_find
-      return count
-    end
-    count += 1 
-  end
-  return nil 
-end
-
 # def find_element_index(array, value_to_find)
-# array.length.times do |count|
-#   if array[count] == value_to_find
-#     return count
+# count = 0 
+#   while count < array.length do
+#     if array[count] == value_to_find
+#       return count
+#     end
+#     count += 1 
 #   end
-#   return nil
-# end 
- 
+#   return nil 
 # end
+
+def find_element_index(array, value_to_find)
+array.length.times do |count|
+  if array[count] == value_to_find
+    return count
+  end
+  
+end 
+  return nil
+ 
+end
 
 def find_max_value(array)
   x = array[0]
